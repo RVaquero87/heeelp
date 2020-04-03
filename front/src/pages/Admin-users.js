18f3b4307cd90b638e9e2f924ed8eb5b3bb66ff5
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-04-03 19:18:56 +0200

    filters control users perfect

--- front/src/pages/Admin-users.js
+++ front/src/pages/Admin-users.js
@@ -6,9 +6,11 @@ import { Loading } from "../components/Loading/index";
 import { UserBoxItem } from "../components/ListItemUser/index";
 
 const AdminUsersPage = () => {
-  const { changeLisUsers } = useContext(PrincipalContext);
+  const { changeLisUsers, setchangeLisUsers } = useContext(PrincipalContext);
 
   const [listUsers, setListUsers] = useState();
+  const [filterlistUsers, setFilterlistUsers] = useState();
+  const [filterStartText, setFilterStartText] = useState("");
 
   const filterNumberLenght = igualToValue =>
     listUsers.filter(user => user.rol === igualToValue).length;
@@ -17,26 +19,68 @@ const AdminUsersPage = () => {
     getListUsers()
       .then(user => {
         setListUsers(user);
+        setFilterlistUsers(user);
       })
       .catch(e => {});
   }, [changeLisUsers]);
 
+  //Filter rol
+  const handleFilterRolUser = async (e, value) => {
+    e.preventDefault();
+    let filter = await listUsers.filter(item => {
+      return item.rol == value;
+    });
+    setFilterlistUsers(filter);
+    if (value === "all") {
+      setchangeLisUsers(!changeLisUsers);
+    }
+  };
+
+  //Filter Text
+  const handleFilterTextUser = async (e, value) => {
+    e.preventDefault();
+    setFilterStartText(value);
+    let filter = await listUsers.filter(item => {
+      const re = new RegExp(value);
+      return re.test(item.name);
+    });
+    setFilterlistUsers(filter);
+  };
+
   //set stado
   return (
     <>
-      {!listUsers ? (
+      {!filterlistUsers ? (
         <Loading />
       ) : (
         <>
+          <p>Filtro</p>
+          <input
+            placeholder="Filtro"
+            value={filterStartText}
+            onChange={e => handleFilterTextUser(e, e.target.value)}
+          />
+          <select onChange={e => handleFilterRolUser(e, e.target.value)}>
+            <option value="all">Todos</option>
+            <option value="Helpers">Helpers</option>
+            <option value="Helped">Helped</option>
+            <option value="Admin">Admin</option>
+          </select>
           <p>NºTotal de uSUARIOS {listUsers.length} </p>
           <p>NºTotal de Admin: {filterNumberLenght("Admin")}</p>
           <p>NºTotal de Helpers: {filterNumberLenght("Helpers")}</p>
           <p>NºTotal de Helped: {filterNumberLenght("Helped")}</p>
-          <div data-aos="fade-up">
-            {listUsers.map((user, i) => {
-              return <UserBoxItem user={user} key={i} />;
-            })}
-          </div>
+          {filterlistUsers.length === 0 ? (
+            <p>No existen nínguna con ese filtro</p>
+          ) : (
+            <>
+              <div data-aos="fade-up">
+                {filterlistUsers.map((user, i) => {
+                  return <UserBoxItem user={user} key={i} />;
+                })}
+              </div>
+            </>
+          )}
         </>
       )}
     </>
