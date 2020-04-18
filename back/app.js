14afbb5b37ee3d31c4699074712806ae87cc9beb
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-04-18 12:14:52 +0200

    change

--- back/app.js
+++ back/app.js
@@ -31,21 +31,21 @@ const debug = require("debug")(
 
 const app = express();
 
-// // Cross Domain CORS whitlist
-// const whitelist = [process.env.FRONT_URL, process.env.BACK_URL];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true,
-// };
+// Cross Domain CORS whitlist
+const whitelist = [process.env.FRONT_URL, process.env.BACK_URL];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
 
-// Middleware Setup
-// app.use(cors(corsOptions));
+//Middleware Setup
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
